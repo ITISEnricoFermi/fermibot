e920ea82db11f412f27369c6e0cecc9dff1fcb3c
Riccardo Sangiorgio <richard.sangiorgio@gmail.com>
2019-02-07 21:26:13 +0100

    Test Express.

--- index.js
+++ index.js
@@ -4,16 +4,22 @@ const { parse } = require('node-html-parser')
 const parseUrl = require('parse-url')
 
 const Telegraf = require('telegraf')
-// const express = require('express')
+const express = require('express')
 
 const { PORT, TOKEN, API, CHANNEL } = process.env
-// const app = express()
+const app = express()
 const bot = new Telegraf(TOKEN)
 const api = new Api(API)
 
-// app.get('/', (req, res) => {
-//   // const posts = api.getPosts(1,1)
-// })
+app.get('/', (req, res) => {
+  console.log(req.body)
+  res.status(200).send()
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.status(200).send()
+})
 
 // api.getPosts(1, 2)
 //   .then(posts => {
@@ -34,11 +40,11 @@ const api = new Api(API)
 //     console.log(e)
 //   })
 
-bot.startWebhook('/', null, PORT)
+// bot.startWebhook('/', null, PORT)
 // bot.handleUpdate(rawUpdate, [webhookResponse])
 
 bot.launch()
 
-// app.listen(PORT, () => {
-//   console.log(`Server started on port ${PORT}.`)
-// })
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}.`)
+})
